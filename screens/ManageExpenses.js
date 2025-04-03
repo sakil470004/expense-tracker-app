328572c8711bcffb9023852af50a3e30c5b360ce
Mynul Islam <mynul.sakil@gmail.com>
2025-04-04 03:06:42 +0600

    141

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -2,14 +2,15 @@ import { useLayoutEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
+import Button from "../components/UI/Button";
 
-function ManageExpenses({route,navigation}) {
+function ManageExpenses({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
   // loading tittle according to the isEditing state
   // we can use useLayoutEffect instead of useEffect to set the title
   // because we want to set the title before the screen is rendered
- useLayoutEffect(() => {
+  useLayoutEffect(() => {
     navigation.setOptions({
       title: isEditing ? 'Edit Expense' : 'Add Expense',
     });
@@ -26,14 +27,17 @@ function ManageExpenses({route,navigation}) {
   }
   return (
     <View style={styles.container}>
-     {
-
-      isEditing && 
-      <View style={styles.deleteContainer}>
-
-      <IconButton icon="trash" color={GlobalStyles.colors.error500} size={24} onPress={deleteExpenseHandler}/>
+      <View style={styles.buttons}>
+        <Button mode='flat' onPress={cancelHandler} style={styles.button} >Cancel</Button>
+        <Button onPress={confirmHandler} style={styles.button}>{isEditing ? 'Update' : 'Add'}</Button>
       </View>
-     }
+      {
+        isEditing &&
+        <View style={styles.deleteContainer}>
+
+          <IconButton icon="trash" color={GlobalStyles.colors.error500} size={24} onPress={deleteExpenseHandler} />
+        </View>
+      }
     </View>
   );
 }
@@ -44,6 +48,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons:{
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
