df86ec0cdaebbaa26a9b49c59119ece1141e7720
Mynul Islam <mynul.sakil@gmail.com>
2025-04-04 01:10:45 +0600

    139

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,6 +1,17 @@
+import { useLayoutEffect } from "react";
 import { Text, View } from "react-native";
 
-function ManageExpenses() {
+function ManageExpenses({route,navigation}) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+  // loading tittle according to the isEditing state
+  // we can use useLayoutEffect instead of useEffect to set the title
+  // because we want to set the title before the screen is rendered
+ useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? 'Edit Expense' : 'Add Expense',
+    });
+  }, [navigation, isEditing]);
   return (
     <View>
       <Text>ManageExpenses Screen</Text>
