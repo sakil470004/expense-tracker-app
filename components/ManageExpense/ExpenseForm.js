cece46466c1a521c49507a68882623351bdbb1b6
Mynul Islam <mynul.sakil@gmail.com>
2025-04-20 23:14:46 +0600

    152

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
 
 function ExpenseForm() {
@@ -6,25 +6,33 @@ function ExpenseForm() {
         // console.log(enteredValue);
     }
     return (
-        <View>
-            <Input label="Amount" textInputConfig={{
-                keyboardType: 'decimal-pad',
-                maxLength: 5,
-                onchangeText: amountChangeHandler,
-                placeholder:'Enter Amount...ex : 10'
-            }} />
-            <Input label="Date"
-                textInputConfig={{
-                    placeholder: 'YYYY-MM-DD',
-                    maxLength: 10,
-                    keyboardType: 'numbers-and-punctuation',
-                    onChangeText: () => { },
-                }}
-            />
+        <View style={styles.form}>
+        <Text style={styles.formTitle}>Your Expense</Text>
+            <View style={styles.inputsRow}>
+                <Input
+                    style={styles.rowInput}
+
+                    label="Amount" textInputConfig={{
+                        keyboardType: 'decimal-pad',
+                        maxLength: 5,
+                        onchangeText: amountChangeHandler,
+                        placeholder: 'Enter Amount...ex : 10'
+                    }} />
+                <Input label="Date"
+                    style={styles.rowInput}
+                    textInputConfig={{
+                        placeholder: 'YYYY-MM-DD',
+                        maxLength: 10,
+                        keyboardType: 'numbers-and-punctuation',
+                        onChangeText: () => { },
+                    }}
+
+                />
+            </View>
             <Input label="Description"
                 textInputConfig={{
                     multiline: true,
-                    placeholder:'Enter Description for your Expense',
+                    placeholder: 'Enter Description for your Expense',
                     // autoCorrect: false,//default is true
                     // autoCapitalize:'none',//default is 'sentences'
                     onChangeText: () => { },
@@ -36,8 +44,26 @@ function ExpenseForm() {
 export default ExpenseForm;
 // style for input
 const styles = StyleSheet.create({
+    form:{
+        marginTop: 100,
+    },
+    formTitle:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
+        marginBottom: 24,
+    },
+    inputsRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 24,
 
-
+    },
+    rowInput: {
+        flex: 1,
+        height: 50,
+    },
 
 });
 
