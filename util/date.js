69642a0ada3141c0a66c19602e6a3a6bd2b694c4
Mynul Islam <mynul.sakil@gmail.com>
2025-04-24 23:49:30 +0600

    fix date problme

--- util/date.js
+++ util/date.js
@@ -1,7 +1,6 @@
 export function getFormattedDate(date) {
    // Array to map day indexes (0 for Sunday, 1 for Monday, etc.) to their abbreviations
    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-   console.log(date?.getDate())
    const dayName = days[date.getDay()];
    
    // Get day, month (add 1 since months are zero-indexed), and year
