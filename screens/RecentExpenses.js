2b8892ca634aa946ce95caf22cf5dceaeaf7361f
Mynul Islam <mynul.sakil@gmail.com>
2025-05-02 03:26:44 +0600

    fixed bug on naming issues

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,11 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
-    const expensesCtx= useContext(ExpensesContext);
-    const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    // const expensesCtx= useContext(ExpensesContext);
+    const [fetchedExpenses, setFetchedExpenses] = useState([]);
+    useEffect(()=>{
+      async function getExpenses(){
+        const expenses = await fetchExpenses();
+        setFetchedExpenses(expenses);
+      }
+      // call the function
+      getExpenses();
+     
+    },[])
+
+    const recentExpenses = fetchedExpenses.filter((expense) => {
         const today = new Date();
         const date7DaysAgo = getDateMinusDays(today, 7);
         return (expense.date >= date7DaysAgo) && (expense.date <= today);
